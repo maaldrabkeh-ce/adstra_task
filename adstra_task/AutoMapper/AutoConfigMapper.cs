1fe5d241544bc5668cbbd91353e9dcfd05c89a1b
Majd Drabkeh <majddrabkeh@outlook.com>
2023-05-15 13:56:36 +0300

    Add Data , AutoMapper

---
+++ adstra_task/AutoMapper/AutoConfigMapper.cs
@@ -0,0 +1,21 @@
+﻿using adstra_task.Models;
+using adstra_task.ViewModels;
+using AutoMapper;
+
+namespace adstra_task.AutoMapper
+{
+    public class AutoConfigMapper
+    {
+        public static IMapper CreateMapper()
+        {
+            var MappConfig = new MapperConfiguration(x =>
+            {
+                x.CreateMap<User, UserViewModel>().ForMember(x => x.UserId, z => z.MapFrom(q => q.Id)).ReverseMap();
+            });
+
+
+            return MappConfig.CreateMapper();
+
+        }
+    }
+}
---
+++ adstra_task/Data/DataDbContext.cs
@@ -0,0 +1,22 @@
+﻿using adstra_task.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace adstra_task.Data
+{
+
+    public class DataDbContext : IdentityDbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
