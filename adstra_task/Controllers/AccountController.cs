ab29b5631f16b7e69c06188141024c830f25f1fa
Majd Drabkeh <majddrabkeh@outlook.com>
2023-05-17 13:48:37 +0300

    Add Account Controller , View

---
+++ adstra_task/Controllers/AccountController.cs
@@ -0,0 +1,102 @@
+﻿using adstra_task.Models;
+using adstra_task.Repository;
+using adstra_task.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace adstra_task.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        private readonly IAccountRepository Account;
+        private readonly UserManager<User> userManager;
+
+        public AccountController(IAccountRepository account, UserManager<User> userManager)
+        {
+            Account = account;
+            this.userManager = userManager;
+        }
+
+
+        [AllowAnonymous]
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+
+        public async Task<IActionResult> Register(UserViewModel model)
+        {
+
+            await Account.Register(model);
+            return RedirectToAction("Dashboard", "Home");
+
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(UserViewModel user)
+        {
+
+            await Account.Login(user);
+            var roles = await Account.GetRole(user);
+            if (roles.Contains("Admin"))
+            {
+                return RedirectToAction("AdminDashboard", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Dashboard", "Home");
+            }
+
+
+        }
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            Account.Logout();
+            return RedirectToAction(nameof(Login));
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> RemoveAdminRole(string email)
+        {
+            var user = await userManager.FindByEmailAsync(email);
+
+            if (user != null)
+            {
+                await userManager.RemoveFromRoleAsync(user, "Admin");
+                await userManager.AddToRoleAsync(user, "User");
+            }
+            return RedirectToAction("Dashboard", "Home");
+        }
+        [Authorize(Roles = "Admin")]
+
+        [HttpPost]
+        public async Task<IActionResult> AddToAdminRole(string email)
+        {
+            var user = await userManager.FindByEmailAsync(email);
+
+            if (user != null)
+            {
+                await userManager.RemoveFromRoleAsync(user, "User");
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+            return RedirectToAction("Dashboard", "Home");
+        }
+
+
+    }
+}
--- adstra_task/Program.cs
+++ adstra_task/Program.cs
@@ -1,7 +1,42 @@
+using adstra_task.Data;
+using adstra_task.Models;
+using adstra_task.Repository;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")),
+    ServiceLifetime.Transient);
+
+//DefaultConnection
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+       .AddCookie(options =>
+       {
+           options.LoginPath = "/Account/Login";
+       });
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+})
+    .AddEntityFrameworkStores<DataDbContext>().AddSignInManager<SignInManager<User>>()
+    .AddRoles<IdentityRole>();
+
+builder.Services.AddScoped<RoleManager<IdentityRole>>();
+
+
+
+
+builder.Services.AddTransient<IAccountRepository, AccountRepository>();
 
 var app = builder.Build();
 
@@ -9,19 +44,15 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Dashboard}/{id?}");
 
 app.Run();
